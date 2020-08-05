910d52d117120019b100f75b1a4cf0a91e777324
Jenkins user <jenkins@ip-10-35-73-249.eu-west-2.compute.internal>
2020-08-05 13:26:45 +0000

    Creating new service merchandise-in-baggage-frontend

---
+++ app/uk/gov/hmrc/merchandiseinbaggagefrontend/controllers/HelloWorldController.scala
@@ -0,0 +1,25 @@
+package uk.gov.hmrc.merchandiseinbaggagefrontend.controllers
+
+import javax.inject.{Inject, Singleton}
+
+import play.api.mvc._
+import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendController
+import uk.gov.hmrc.merchandiseinbaggagefrontend.config.AppConfig
+import uk.gov.hmrc.merchandiseinbaggagefrontend.views.html.HelloWorldPage
+
+import scala.concurrent.Future
+
+@Singleton
+class HelloWorldController @Inject()(
+  appConfig: AppConfig,
+  mcc: MessagesControllerComponents,
+  helloWorldPage: HelloWorldPage)
+    extends FrontendController(mcc) {
+
+  implicit val config: AppConfig = appConfig
+
+  val helloWorld: Action[AnyContent] = Action.async { implicit request =>
+    Future.successful(Ok(helloWorldPage()))
+  }
+
+}
