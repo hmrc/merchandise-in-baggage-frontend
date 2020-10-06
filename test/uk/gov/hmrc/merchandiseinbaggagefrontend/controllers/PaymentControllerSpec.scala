c2b938e15bba211f595129eaba97d70b6a9de971
Pasquale Gatto <47112536+PasGat@users.noreply.github.com>
2020-10-06 11:51:02 +0100

    MIB-96: Add form binding to tests

--- test/uk/gov/hmrc/merchandiseinbaggagefrontend/controllers/PaymentControllerSpec.scala
+++ test/uk/gov/hmrc/merchandiseinbaggagefrontend/controllers/PaymentControllerSpec.scala
@@ -22,6 +22,7 @@ import play.api.test.FakeRequest
 import play.api.test.Helpers._
 import uk.gov.hmrc.http.{HeaderCarrier, HttpClient, HttpResponse}
 import uk.gov.hmrc.merchandiseinbaggagefrontend.config.ErrorHandler
+import uk.gov.hmrc.merchandiseinbaggagefrontend.forms.CheckYourAnswersFormProvider
 import uk.gov.hmrc.merchandiseinbaggagefrontend.model.api.PayApitRequest
 import uk.gov.hmrc.merchandiseinbaggagefrontend.views.html.{ErrorTemplate, PaymentPage}
 
@@ -34,45 +35,47 @@ class PaymentControllerSpec extends DeclarationJourneyControllerSpec {
   private lazy val httpClient = app.injector.instanceOf[HttpClient]
   private lazy val component = app.injector.instanceOf[MessagesControllerComponents]
   private lazy val errorHandlerTemplate = app.injector.instanceOf[ErrorTemplate]
-
+  private lazy val formProvider = app.injector.instanceOf[CheckYourAnswersFormProvider]
   private implicit lazy val errorHandler: ErrorHandler = app.injector.instanceOf[ErrorHandler]
 
   private def messages[A](fakeRequest: FakeRequest[A]): Messages = messagesApi.preferred(fakeRequest)
 
   "on page load will render PaymentPage template" in {
-    val controller = new PaymentController(component, view, httpClient)
+    val controller = new PaymentController(component, view, formProvider, httpClient)
     val getRequest = buildGet(routes.PaymentController.onPageLoad().url)
 
     contentAsString(controller.onPageLoad(getRequest)) mustBe view()(getRequest, messages(getRequest), appConfig).toString
   }
 
-  "on submit will trigger a call to pay-api and render the response" in {
+  "on submit will trigger a call to pay-api to make payment and render the response" in {
     val stubbedApiResponse = s"""{"journeyId":"5f3b","nextUrl":"http://host"}"""
-    val controller = new PaymentController(component, view, httpClient) {
-      override def makePayment(httpClient: HttpClient, requestBody: PayApitRequest)(implicit hc: HeaderCarrier, ec: ExecutionContext): Future[HttpResponse] =
+    val controller = new PaymentController(component, view, formProvider, httpClient) {
+      override def makePayment(httpClient: HttpClient, requestBody: PayApitRequest)
+                              (implicit hc: HeaderCarrier, ec: ExecutionContext): Future[HttpResponse] =
         Future.successful(HttpResponse(201, stubbedApiResponse))
     }
 
-    val getRequest = buildGet(routes.PaymentController.onSubmit().url)
-    val eventualResult = controller.onSubmit()(getRequest)
+    val postRequest = buildPost(routes.PaymentController.onSubmit().url)
+      .withFormUrlEncodedBody("taxDue" -> "10")
+    val eventualResult = controller.onSubmit()(postRequest)
 
     status(eventualResult) mustBe 303
     redirectLocation(eventualResult) mustBe Some("http://host")
   }
 
   "on submit will return error page if call to pay-api fails" in {
-    val controller = new PaymentController(component, view, httpClient) {
+    val controller = new PaymentController(component, view, formProvider, httpClient) {
       override def makePayment(httpClient: HttpClient, requestBody: PayApitRequest)(implicit hc: HeaderCarrier, ec: ExecutionContext): Future[HttpResponse] =
         Future.failed(new Exception("Something wrong"))
     }
 
-    val getRequest = buildGet(routes.PaymentController.onSubmit().url)
-    val eventualResult = controller.onSubmit()(getRequest)
+    val postRequest = buildPost(routes.PaymentController.onSubmit().url).withFormUrlEncodedBody("taxDue" -> "10")
+    val eventualResult = controller.onSubmit()(postRequest)
 
     status(eventualResult) mustBe 500
     contentAsString(eventualResult) mustBe errorHandlerTemplate(
       "Sorry, we are experiencing technical difficulties - 500",
       "Sorry, we’re experiencing technical difficulties",
-      "Please try again in a few minutes.")(getRequest, messages(getRequest), appConfig).toString
+      "Please try again in a few minutes.")(postRequest, messages(postRequest), appConfig).toString
   }
 }
