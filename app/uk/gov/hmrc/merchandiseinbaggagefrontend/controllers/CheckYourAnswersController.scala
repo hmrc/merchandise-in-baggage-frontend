d8138224c0dc72836b6bf406fcf5c5001e21dbf1
Pasquale Gatto <47112536+PasGat@users.noreply.github.com>
2020-10-06 11:56:02 +0100

    rebasing

--- app/uk/gov/hmrc/merchandiseinbaggagefrontend/controllers/CheckYourAnswersController.scala
+++ app/uk/gov/hmrc/merchandiseinbaggagefrontend/controllers/CheckYourAnswersController.scala
@@ -33,6 +33,6 @@ class CheckYourAnswersController @Inject()(override val controllerComponents: Me
   val form: Form[Answers] = formProvider()
 
   val onPageLoad: Action[AnyContent] = actionProvider.journeyAction { implicit request =>
-    request.declarationJourney.toDeclarationIfComplete.fold(actionProvider.invalidRequest)(declaration => Ok(page(declaration)))
+    request.declarationJourney.toDeclarationIfComplete.fold(actionProvider.invalidRequest)(declaration => Ok(page(form, declaration)))
   }
 }
