5617d837d1598f63968ca72119ce7f1290f4e9a1
Pasquale Gatto <47112536+PasGat@users.noreply.github.com>
2020-10-09 12:23:33 +0100

    MIB-115: extract custom agent out skeleton

---
+++ app/uk/gov/hmrc/merchandiseinbaggagefrontend/controllers/CustomsAgentController.scala
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2020 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.merchandiseinbaggagefrontend.controllers
+
+import javax.inject.Inject
+import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
+import uk.gov.hmrc.merchandiseinbaggagefrontend.forms.GoodsDestinationFormProvider
+
+import scala.concurrent.ExecutionContext
+
+class CustomsAgentController @Inject()(
+                                        override val controllerComponents: MessagesControllerComponents,
+                                        actionProvider: DeclarationJourneyActionProvider,
+                                        formProvider: GoodsDestinationFormProvider,
+                                      )(implicit ec: ExecutionContext) extends DeclarationJourneyUpdateController {
+
+  override val onSubmit: Action[AnyContent] = customsAgentSubmit
+  override val onPageLoad: Action[AnyContent] = customsAgent
+
+  private def customsAgent: Action[AnyContent] = actionProvider.journeyAction { implicit request =>
+    Ok("hello")
+  }
+
+  private def customsAgentSubmit: Action[AnyContent] = actionProvider.journeyAction { implicit request =>
+    Ok("hello submit")
+  }
+}
---
+++ test/uk/gov/hmrc/merchandiseinbaggagefrontend/controllers/CustomsAgentControllerSpec.scala
@@ -0,0 +1,44 @@
+/*
+ * Copyright 2020 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.merchandiseinbaggagefrontend.controllers
+
+import play.api.test.Helpers._
+
+class CustomsAgentControllerSpec extends DeclarationJourneyControllerSpec {
+
+  private lazy val controller = app.injector.instanceOf[CustomsAgentController]
+  val url = routes.SkeletonJourneyController.customsAgent().url
+
+  "render the page when a declaration journey has been started" in {
+    val title = "Are you a customs agent?"
+    val getRequest = buildGet(url, sessionId)
+
+    givenADeclarationJourneyIsPersisted(startedDeclarationJourney)
+    val eventualResponse = controller.onPageLoad()(getRequest)
+    val content = contentAsString(eventualResponse)
+
+    status(eventualResponse) mustBe OK
+//    content must include(title)
+  }
+
+//  "redirect to /invalid-request" when {
+//    "a declaration journey has not been started" in {
+//      ensureRedirectToInvalidRequestPage(controller.customsAgent(buildGet(url)))
+//    }
+//  }
+
+}
