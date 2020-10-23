8b2be8eb84deb1ac670fe8868a80bb783454198f
jordanrowe <37838443+jordanrowe@users.noreply.github.com>
2020-10-23 09:49:11 +0100

    add vat rate to review-goods

---
+++ app/uk/gov/hmrc/merchandiseinbaggagefrontend/model/NavMode.scala
@@ -0,0 +1,5 @@
+package uk.gov.hmrc.merchandiseinbaggagefrontend.model
+
+trait NavMode {
+
+}
---
+++ app/uk/gov/hmrc/merchandiseinbaggagefrontend/viewmodels/ReviewGoodsSummary.scala
@@ -0,0 +1,5 @@
+package uk.gov.hmrc.merchandiseinbaggagefrontend.viewmodels
+
+object ReviewGoodsSummary {
+
+}
