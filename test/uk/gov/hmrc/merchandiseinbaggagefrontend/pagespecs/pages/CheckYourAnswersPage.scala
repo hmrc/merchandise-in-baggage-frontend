a95828f3445eb070e2eb4eb5637400a50ca49749
Pasquale Gatto <47112536+PasGat@users.noreply.github.com>
2020-11-09 13:54:18 +0000

    MIB-142_V3: move assertion in Spec step#3

--- test/uk/gov/hmrc/merchandiseinbaggagefrontend/pagespecs/pages/CheckYourAnswersPage.scala
+++ test/uk/gov/hmrc/merchandiseinbaggagefrontend/pagespecs/pages/CheckYourAnswersPage.scala
@@ -17,83 +17,11 @@
 package uk.gov.hmrc.merchandiseinbaggagefrontend.pagespecs.pages
 
 import org.openqa.selenium.WebDriver
-import org.scalatest.Assertion
 import org.scalatestplus.selenium.WebBrowser
-import uk.gov.hmrc.merchandiseinbaggagefrontend.model.core.{AmountInPence, Declaration, JourneyInSmallVehicle}
-import uk.gov.hmrc.merchandiseinbaggagefrontend.pagespecs.pages.CheckYourAnswersPage.expectedSectionHeaders
 
 class CheckYourAnswersPage(implicit webDriver: WebDriver) extends PageWithCTA {
 
   import WebBrowser._
-
-  def mustRenderDetail(declaration: Declaration, totalTaxDue: AmountInPence): Unit = patiently {
-    findAll(TagNameQuery("h2")).map(_.underlying.getText).toSeq.dropRight(1) mustBe expectedSectionHeaders
-
-    def textOfElementWithId(id: String): String = find(IdQuery(id)).get.underlying.getText
-
-    def elementIsNotRenderedWithId(id: String): Assertion = find(IdQuery(id)).isEmpty mustBe true
-
-    declaration.declarationGoods.goods.zipWithIndex.foreach { goodsWithIndex =>
-      val goods = goodsWithIndex._1
-      val index = goodsWithIndex._2
-
-      textOfElementWithId(s"categoryLabel_$index") mustBe "Type of goods"
-      textOfElementWithId(s"category_$index") mustBe goods.categoryQuantityOfGoods.category
-
-      textOfElementWithId(s"quantityLabel_$index") mustBe "Number of items"
-      textOfElementWithId(s"quantity_$index") mustBe goods.categoryQuantityOfGoods.quantity
-
-      textOfElementWithId(s"countryLabel_$index") mustBe "Country"
-      textOfElementWithId(s"country_$index") mustBe goods.countryOfPurchase
-
-      textOfElementWithId(s"priceLabel_$index") mustBe "Price paid"
-      textOfElementWithId(s"price_$index") mustBe goods.purchaseDetails.toString
-
-      textOfElementWithId(s"invoiceNumberLabel_$index") mustBe "Invoice number"
-      textOfElementWithId(s"invoiceNumber_$index") mustBe goods.invoiceNumber
-    }
-
-    textOfElementWithId("taxDueLabel") mustBe "Tax due"
-    textOfElementWithId("taxDueValue") mustBe totalTaxDue.formattedInPounds
-
-    declaration.maybeCustomsAgent.fold {
-      elementIsNotRenderedWithId("customsAgentNameLabel")
-      elementIsNotRenderedWithId("customsAgentName")
-
-      elementIsNotRenderedWithId("customsAgentAddressLabel")
-      elementIsNotRenderedWithId("customsAgentAddress")
-    } { customsAgent =>
-      textOfElementWithId("customsAgentNameLabel") mustBe "Name of customs agent"
-      textOfElementWithId("customsAgentName") mustBe customsAgent.name
-
-      textOfElementWithId("customsAgentAddressLabel") mustBe "Customs agent address"
-      textOfElementWithId("customsAgentAddress").contains(customsAgent.address.postcode.get) mustBe true
-    }
-
-    textOfElementWithId("eoriLabel") mustBe "EORI number"
-    textOfElementWithId("eori") mustBe declaration.eori.toString
-
-    textOfElementWithId("nameOfPersonCarryingTheGoodsLabel") mustBe "Name of person carrying goods"
-    textOfElementWithId("nameOfPersonCarryingTheGoods") mustBe declaration.nameOfPersonCarryingTheGoods.toString
-
-    textOfElementWithId("placeOfArrivalLabel") mustBe "Place of arrival"
-    textOfElementWithId("placeOfArrival") mustBe declaration.journeyDetails.placeOfArrival.display
-
-    textOfElementWithId("dateOfArrivalLabel") mustBe "Date of arrival"
-    textOfElementWithId("dateOfArrival") mustBe declaration.journeyDetails.formattedDateOfArrival
-
-    textOfElementWithId("travellingByVehicleLabel") mustBe "Travelling by vehicle"
-    textOfElementWithId("travellingByVehicle") mustBe declaration.journeyDetails.travellingByVehicle.entryName
-
-    declaration.journeyDetails match {
-      case journeyInSmallVehicle: JourneyInSmallVehicle =>
-        textOfElementWithId("vehicleRegistrationNumberLabel") mustBe "Vehicle registration number"
-        textOfElementWithId("vehicleRegistrationNumber") mustBe journeyInSmallVehicle.registrationNumber
-      case _ =>
-        elementIsNotRenderedWithId("vehicleRegistrationNumberLabel")
-        elementIsNotRenderedWithId("vehicleRegistrationNumber")
-    }
-  }
 
   def mustRedirectToPaymentFromTheCTA(): Unit = {
     val button = find(NameQuery("payButton")).get
