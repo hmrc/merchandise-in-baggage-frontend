9d319e282b8ed87721b5696c4ad1f63cd15fed78
Pasquale Gatto <47112536+PasGat@users.noreply.github.com>
2020-12-04 10:43:26 +0000

    Mib 259 (#163)
    * MIB-259: fix bug and add tests
    
    * MIB-259: add tests #2
    
    * MIB-259: cleaning up
    
    * MIB-259: cleaning up #2
    
    * MIB-259: cleaning up #3
    
    * MIB-259: more tests

---
+++ test/uk/gov/hmrc/merchandiseinbaggage/controllers/RemoveGoodsControllerSpec.scala
@@ -0,0 +1,75 @@
+/*
+ * Copyright 2020 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.merchandiseinbaggage.controllers
+
+import play.api.test.Helpers._
+import play.mvc.Http.Status
+import uk.gov.hmrc.merchandiseinbaggage.CoreTestData
+import uk.gov.hmrc.merchandiseinbaggage.model.core.GoodsEntries
+import uk.gov.hmrc.merchandiseinbaggage.model.core.YesNo._
+import uk.gov.hmrc.merchandiseinbaggage.repositories.DeclarationJourneyRepository
+import uk.gov.hmrc.merchandiseinbaggage.views.html.RemoveGoodsView
+
+import scala.concurrent.ExecutionContext.Implicits.global
+
+class RemoveGoodsControllerSpec extends DeclarationJourneyControllerSpec with CoreTestData {
+
+  val repo = app.injector.instanceOf[DeclarationJourneyRepository]
+  val view = app.injector.instanceOf[RemoveGoodsView]
+  val controller = new RemoveGoodsController(controllerComponents, actionBuilder, repo, view)
+
+  "on submit if answer No" should {
+    s"redirect back to ${routes.CheckYourAnswersController.onPageLoad().url} if journey was completed" in {
+      val result = controller.removeGoodOrRedirect(1, completedDeclarationJourney, No)
+
+      status(result) mustBe Status.SEE_OTHER
+      redirectLocation(result) mustBe Some(routes.CheckYourAnswersController.onPageLoad().url)
+    }
+
+    s"redirect back to ${routes.ReviewGoodsController.onPageLoad().url} if journey was NOT completed" in {
+      val result = controller.removeGoodOrRedirect(1, startedImportJourney, No)
+
+      status(result) mustBe Status.SEE_OTHER
+      redirectLocation(result) mustBe Some(routes.ReviewGoodsController.onPageLoad().url)
+    }
+  }
+
+  "on submit if answer yes" should {
+    s"redirect to ${routes.GoodsRemovedController.onPageLoad()} if goods contains an entry" in {
+      val result = controller.removeGoodOrRedirect(1, importJourneyWithStartedGoodsEntry, Yes)
+
+      status(result) mustBe Status.SEE_OTHER
+      redirectLocation(result) mustBe Some(routes.GoodsRemovedController.onPageLoad().url)
+    }
+
+    s"redirect to ${routes.ReviewGoodsController.onPageLoad()} if goods contains more entries" in {
+      val journey = importJourneyWithStartedGoodsEntry.copy(goodsEntries = GoodsEntries(Seq(startedGoodsEntry, startedGoodsEntry)))
+      val result = controller.removeGoodOrRedirect(1, journey, Yes)
+
+      status(result) mustBe Status.SEE_OTHER
+      redirectLocation(result) mustBe Some(routes.ReviewGoodsController.onPageLoad().url)
+    }
+
+    s"redirect to ${routes.CheckYourAnswersController.onPageLoad()} if goods contains more entries and is completed" in {
+      val journey = completedDeclarationJourney.copy(goodsEntries = GoodsEntries(Seq(completedGoodsEntry, completedGoodsEntry)))
+      val result = controller.removeGoodOrRedirect(1, journey, Yes)
+
+      status(result) mustBe Status.SEE_OTHER
+      redirectLocation(result) mustBe Some(routes.CheckYourAnswersController.onPageLoad().url)
+    }
+  }
+}
