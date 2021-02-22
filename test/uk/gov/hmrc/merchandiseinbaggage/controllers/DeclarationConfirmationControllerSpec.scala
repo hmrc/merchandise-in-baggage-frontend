bc548ba1925deada6b083ba6824deef2245738f8
Andy Hicks <455080+andy1138@users.noreply.github.com>
2021-02-22 08:06:40 +0000

    [MIBM-465] Add optional 'take proof' message

--- test/uk/gov/hmrc/merchandiseinbaggage/controllers/DeclarationConfirmationControllerSpec.scala
+++ test/uk/gov/hmrc/merchandiseinbaggage/controllers/DeclarationConfirmationControllerSpec.scala
@@ -17,13 +17,14 @@
 package uk.gov.hmrc.merchandiseinbaggage.controllers
 
 import java.time.LocalDateTime
-
+import play.api.mvc.Result
 import play.api.test.Helpers._
 import uk.gov.hmrc.http.{HeaderCarrier, HttpClient}
 import uk.gov.hmrc.merchandiseinbaggage.config.MibConfiguration
 import uk.gov.hmrc.merchandiseinbaggage.connectors.MibConnector
 import uk.gov.hmrc.merchandiseinbaggage.model.api.{Declaration, DeclarationId}
 import uk.gov.hmrc.merchandiseinbaggage.model.api._
+import uk.gov.hmrc.merchandiseinbaggage.model.api.calculation.{CalculationResult, CalculationResults}
 import uk.gov.hmrc.merchandiseinbaggage.model.core.DeclarationJourney
 import uk.gov.hmrc.merchandiseinbaggage.stubs.MibBackendStub._
 import uk.gov.hmrc.merchandiseinbaggage.views.html.DeclarationConfirmationView
@@ -79,4 +80,65 @@ class DeclarationConfirmationControllerSpec extends DeclarationJourneyController
     status(eventualResult) mustBe 303
     redirectLocation(eventualResult) mustBe Some("/declare-commercial-goods/cannot-access-page")
   }
+
+  private def generateDeclarationConfirmationPage(decType: DeclarationType, purchaseAmount: Long): Future[Result] = {
+    val dummyAmount = AmountInPence(0)
+
+    def totalCalculationResult: TotalCalculationResult =
+      TotalCalculationResult(
+        CalculationResults(Seq(CalculationResult(
+          ImportGoods(CategoryQuantityOfGoods("sock", "1"),
+            GoodsVatRates.Twenty,
+            YesNoDontKnow.Yes,
+            PurchaseDetails(purchaseAmount.toString, Currency("GBP", "title.british_pounds_gbp", None, List.empty[String]))
+          ),
+          AmountInPence(purchaseAmount), dummyAmount, dummyAmount, None))),
+        AmountInPence(purchaseAmount),
+        dummyAmount,
+        dummyAmount,
+        dummyAmount
+      )
+
+    val sessionId = SessionId()
+    val id = DeclarationId("456")
+    val created = LocalDateTime.now.withSecond(0).withNano(0)
+    val request = buildGet(routes.DeclarationConfirmationController.onPageLoad().url, sessionId)
+
+    val journey: DeclarationJourney = completedDeclarationJourney
+      .copy(sessionId = sessionId, declarationType = decType, createdAt = created, declarationId = id)
+
+    givenADeclarationJourneyIsPersisted(journey)
+
+    val persistedDeclaration = journey.declarationIfRequiredAndComplete.map{ x =>
+      if(decType==DeclarationType.Import ) x.copy(maybeTotalCalculationResult = Some(totalCalculationResult)) else x}
+
+    givenPersistedDeclarationIsFound(persistedDeclaration.get, id)
+
+    controller.onPageLoad()(request)
+  }
+
+  "Import with value over 1000gbp, add an 'take proof' line" in {
+    val eventualResult = generateDeclarationConfirmationPage(DeclarationType.Import, 111111)
+    status(eventualResult) mustBe 200
+
+    val bodyResult = contentAsString(eventualResult)
+    bodyResult must include(messageApi("declarationConfirmation.ul.3"))
+  }
+
+  "Import with value under 1000gbp, DONOT add an 'take proof' line" in {
+    val eventualResult = generateDeclarationConfirmationPage(DeclarationType.Import, 100)
+    status(eventualResult) mustBe 200
+
+    val bodyResult = contentAsString(eventualResult)
+    bodyResult mustNot include(messageApi("declarationConfirmation.ul.3"))
+  }
+
+  "Export with value over 1000gbp, DONOT add an 'take proof' line" in {
+    val eventualResult = generateDeclarationConfirmationPage(DeclarationType.Export, 111111)
+    status(eventualResult) mustBe 200
+
+    val bodyResult = contentAsString(eventualResult)
+    bodyResult mustNot include(messageApi("declarationConfirmation.ul.3"))
+  }
+
 }
