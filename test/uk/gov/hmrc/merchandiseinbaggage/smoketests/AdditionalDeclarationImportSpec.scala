6b4bed1e0e6ecc868356b7dc5ee046082688c09c
Andy Hicks <455080+andy1138@users.noreply.github.com>
2021-04-07 14:04:04 +0100

    [MIBM-574] working smoketest for Additional Declaration Import

--- test/uk/gov/hmrc/merchandiseinbaggage/smoketests/AdditionalDeclarationImportSpec.scala
+++ test/uk/gov/hmrc/merchandiseinbaggage/smoketests/AdditionalDeclarationImportSpec.scala
@@ -16,14 +16,11 @@
 
 package uk.gov.hmrc.merchandiseinbaggage.smoketests
 
-import uk.gov.hmrc.merchandiseinbaggage.model.api.DeclarationType.Import
 import uk.gov.hmrc.merchandiseinbaggage.model.api.YesNo.{No, Yes}
-import uk.gov.hmrc.merchandiseinbaggage.model.api.{CategoryQuantityOfGoods, DeclarationType, Paid, SessionId}
-import uk.gov.hmrc.merchandiseinbaggage.model.core.{DeclarationJourney, PurchaseDetailsInput, RetrieveDeclaration}
-import uk.gov.hmrc.merchandiseinbaggage.smoketests.pages.{CheckYourAnswersPage, ExciseAndRestrictedGoodsPage, GoodsOriginPage, GoodsTypeQuantityPage, GoodsVatRatePage, NeworExistingDeclarationPage, PreviousDeclarationDetailsPage, PurchaseDetailsPage, RetrieveDeclarationPage, ReviewGoodsPage, StartImportPage, ValueWeightOfGoodsPage}
+import uk.gov.hmrc.merchandiseinbaggage.model.api.{CategoryQuantityOfGoods, Paid}
+import uk.gov.hmrc.merchandiseinbaggage.model.core.{ PurchaseDetailsInput, RetrieveDeclaration}
+import uk.gov.hmrc.merchandiseinbaggage.smoketests.pages.{CheckYourAnswersPage, ExciseAndRestrictedGoodsPage, GoodsOriginPage, GoodsTypeQuantityPage, GoodsVatRatePage, NeworExistingDeclarationPage, PaymentCalculationPage, PreviousDeclarationDetailsPage, PurchaseDetailsPage, RetrieveDeclarationPage, ReviewGoodsPage, StartImportPage, ValueWeightOfGoodsPage}
 import uk.gov.hmrc.merchandiseinbaggage.stubs.MibBackendStub._
-
-import java.time.LocalDateTime
 
 class AdditionalDeclarationImportSpec extends BaseUiSpec {
 
@@ -33,27 +30,15 @@ class AdditionalDeclarationImportSpec extends BaseUiSpec {
 
       submitPage(NeworExistingDeclarationPage, "Amend")
 
-      val paidDeclaration = declaration.copy(paymentStatus = Some(Paid))
+      val paidDeclaration = declaration.copy(
+        paymentStatus = Some(Paid),
+        maybeTotalCalculationResult = Some(aTotalCalculationResult),
+        eori = eori,
+        mibReference = mibReference)
 
       givenFindByDeclarationReturnSuccess(mibReference, eori, paidDeclaration)
 
-      val sessionId = SessionId()
-      val created = LocalDateTime.now
-      val id = paidDeclaration.declarationId
-      val importJourney: DeclarationJourney = completedDeclarationJourney
-        .copy(
-          sessionId = sessionId,
-          declarationType = DeclarationType.Import,
-          maybeEori = Some(eori),
-          createdAt = created,
-          declarationId = id)
-
-      givenADeclarationJourneyIsPersisted(importJourney)
-      givenDeclarationIsAmendedInBackend
-      givenPersistedDeclarationIsFound(importJourney.declarationIfRequiredAndComplete.get, id)
-
-      givenDeclarationIsPersistedInBackend(declaration)
-      givenDeclarationIsPersistedInBackend
+      givenPersistedDeclarationIsFound(paidDeclaration, paidDeclaration.declarationId)
 
       givenAPaymentCalculation(aCalculationResult)
       givenEoriIsChecked(eori.toString)
@@ -67,7 +52,6 @@ class AdditionalDeclarationImportSpec extends BaseUiSpec {
 
       submitPage(PreviousDeclarationDetailsPage, "continue")
 
-      // controlled or restricted goods
       submitPage(ExciseAndRestrictedGoodsPage, No)
 
       submitPage(ValueWeightOfGoodsPage, Yes)
@@ -83,16 +67,12 @@ class AdditionalDeclarationImportSpec extends BaseUiSpec {
       webDriver.getPageSource must include("Yes")
       webDriver.getPageSource must include("100.50, Euro (EUR)")
 
-
       submitPage(ReviewGoodsPage, "No")
 
-      println(">>>" + webDriver.getPageSource)
+      submitPage(PaymentCalculationPage, "")
 
-      webDriver.getPageSource mustNot include("Sorry, we’re experiencing technical difficulties")
-
-      submitPage(CheckYourAnswersPage, Import)
-
-      webDriver.getCurrentUrl mustBe fullUrl(PreviousDeclarationDetailsPage.path)
+      webDriver.getPageSource must include("payButton")
+      webDriver.getCurrentUrl mustBe fullUrl(CheckYourAnswersPage.path)
     }
   }
 }
