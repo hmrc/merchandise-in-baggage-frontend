3a1f066443618cb332d7d5b8c4b6e4fb4c6e7f0a
Pasquale Gatto <47112536+PasGat@users.noreply.github.com>
2021-04-28 09:09:30 +0100

    MIB-607: content changes Export

--- test/uk/gov/hmrc/merchandiseinbaggage/content/PurchaseDetailsContentSpec.scala
+++ test/uk/gov/hmrc/merchandiseinbaggage/content/PurchaseDetailsContentSpec.scala
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2021 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package uk.gov.hmrc.merchandiseinbaggage.content
 
 import uk.gov.hmrc.merchandiseinbaggage.CoreTestData
@@ -6,11 +22,13 @@ import uk.gov.hmrc.merchandiseinbaggage.smoketests.pages.PurchaseDetailsPage
 
 class PurchaseDetailsContentSpec extends PurchaseDetailsPage with CoreTestData with PropertyBaseTables {
 
-  s"render contents" in {
-    givenAJourneyWithSession()
-    goToPurchaseDetailsPage(1)
+  forAll(declarationTypesTable) { importOrExport =>
+    s"render contents for $importOrExport" in {
+      givenAJourneyWithSession(declarationType = importOrExport)
+      goToPurchaseDetailsPage(1)
 
-    elementText(findByTagName("h1")) must include(messages("purchaseDetails.heading", "wine"))
-    elementText(findByClassName("govuk-inset-text")) must include(messages("purchaseDetails.type.of.goods", "wine"))
+      elementText(findByTagName("h1")) must include(messages("purchaseDetails.heading", "wine"))
+      elementText(findByClassName("govuk-inset-text")) must include(messages("purchaseDetails.type.of.goods", "wine"))
+    }
   }
 }
