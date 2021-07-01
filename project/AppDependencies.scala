57eea8d78fdea49d5cfefa8b7cad02c5650a05dc
Pasquale Gatto <47112536+PasGat@users.noreply.github.com>
2021-07-01 11:12:39 +0100

    [DDCE-1992][PG]: lib upgrades

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,14 +3,14 @@ import sbt._
 
 object AppDependencies {
 
-  private val pactVersion = "3.2.0"
+  private val pactVersion = "3.2.1"
 
   val compile = Seq(
     "uk.gov.hmrc"                %% "bootstrap-frontend-play-28" % "5.3.0",
     "uk.gov.hmrc"                %% "play-frontend-hmrc"         % "0.69.0-play-28",
     "uk.gov.hmrc"                %% "simple-reactivemongo"       % "8.0.0-play-28",
     "com.github.pureconfig"      %% "pureconfig"                 % "0.13.0",
-    "com.beachape"               %% "enumeratum-play"            % "1.5.13",
+    "com.beachape"               %% "enumeratum-play"            % "1.5.17",
     "org.webjars.npm"            %  "accessible-autocomplete"    % "2.0.3",
     "uk.gov.hmrc"                %% "play-language"              % "5.1.0-play-28",
     "com.softwaremill.quicklens" %% "quicklens"                  % "1.6.1",
@@ -32,11 +32,11 @@ object AppDependencies {
     "com.itv"                  %% "scalapact-http4s-0-21"   % pactVersion    % Test,
     "com.itv"                  %% "scalapact-scalatest"     % pactVersion    % Test,
     "org.scalaj"               %% "scalaj-http"             % "2.4.2"    % Test,
-    "org.json4s"               %% "json4s-native"           % "3.6.9"    % Test
+    "org.json4s"               %% "json4s-native"           % "3.6.11"    % Test
   )
 
   private lazy val mocks = Seq(
     "org.scalamock"            %% "scalamock" % "5.1.0"     % Test,
-    "com.github.tomakehurst"   %  "wiremock-standalone"     % "2.27.1"   % Test
+    "com.github.tomakehurst"   %  "wiremock-standalone"     % "2.27.2"   % Test
   )
 }
