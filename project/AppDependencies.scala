e5e4c02c7585997fb9e9fba216249ff17e30616e
Matthew Ward <96430760+matthewwardcap@users.noreply.github.com>
2023-04-17 12:55:16 +0100

    [DDCE-3777] Dependencies + Scala 2.13 upgrade

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,18 +4,18 @@ import sbt._
 object AppDependencies {
 
   private val pactVersion = "4.4.0"
-  private val bootstrapVersion = "7.13.0"
+  private val bootstrapVersion = "7.15.0"
   private val akkaVersion = "2.6.20"
 
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"                 %% "bootstrap-frontend-play-28" % bootstrapVersion,
-    "uk.gov.hmrc"                 %% "auth-client"                % "5.14.0-play-28",
+    "uk.gov.hmrc"                 %% "auth-client"                % "6.0.0-play-28",
     "uk.gov.hmrc"                 %% "play-frontend-hmrc"         % "6.2.0-play-28",
     "uk.gov.hmrc.mongo"           %% "hmrc-mongo-play-28"         % "0.74.0",
-    "com.github.pureconfig"       %% "pureconfig"                 % "0.17.2",
+    "com.github.pureconfig"       %% "pureconfig"                 % "0.17.3",
     "com.beachape"                %% "enumeratum-play"            % "1.7.0",
     "org.webjars.npm"             %  "accessible-autocomplete"    % "2.0.4",
-    "com.softwaremill.quicklens"  %% "quicklens"                  % "1.9.0",
+    "com.softwaremill.quicklens"  %% "quicklens"                  % "1.9.2",
     "org.typelevel"               %% "cats-core"                  % "2.9.0",
     "com.typesafe.akka"           %% "akka-protobuf"              % akkaVersion,
     "com.typesafe.akka"           %% "akka-actor-typed"           % akkaVersion,
@@ -28,7 +28,7 @@ object AppDependencies {
     "uk.gov.hmrc"            %% "bootstrap-test-play-28" % bootstrapVersion,
     "org.scalatest"          %% "scalatest"              % "3.2.15",
     "com.typesafe.play"      %% "play-test"              % current,
-    "com.vladsch.flexmark"   %  "flexmark-all"           % "0.62.2",
+    "com.vladsch.flexmark"   %  "flexmark-all"           % "0.64.0",
     "org.scalatestplus.play" %% "scalatestplus-play"     % "5.1.0",
     "org.scalatestplus"      %% "scalacheck-1-17"        % "3.2.15.0",
     "org.scalatestplus"      %% "selenium-4-7"           % "3.2.15.0"
@@ -42,8 +42,8 @@ object AppDependencies {
   )
 
   private lazy val mocks = Seq(
-    "org.scalatestplus"      %% "mockito-3-4"         % "3.2.10.0",
-    "org.scalamock"          %% "scalamock"           % "5.2.0",
-    "com.github.tomakehurst" %  "wiremock-standalone" % "2.27.2"
+    "org.mockito"             %% "mockito-scala-scalatest"  % "1.17.14",
+    "org.scalamock"           %% "scalamock"                % "5.2.0",
+    "com.github.tomakehurst"  %  "wiremock-standalone"      % "2.27.2"
   )
 }
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -3,19 +3,23 @@ resolvers += Resolver.url("HMRC-open-artefacts-ivy", url("https://open.artefacts
 
 resolvers += Resolver.typesafeRepo("releases")
 
+// To resolve a bug with version 2.x.x of the scoverage plugin - https://github.com/sbt/sbt/issues/6997
+// Try to remove when sbt 1.8.0+ and scoverage is 2.0.7+
+ThisBuild / libraryDependencySchemes ++= Seq("org.scala-lang.modules" %% "scala-xml" % VersionScheme.Always)
+
 addSbtPlugin("uk.gov.hmrc" % "sbt-auto-build" % "3.9.0")
 
 addSbtPlugin("uk.gov.hmrc" % "sbt-distributables" % "2.2.0")
 
 addSbtPlugin("com.typesafe.play" % "sbt-plugin" % "2.8.19")
 
-addSbtPlugin("org.irundaia.sbt" % "sbt-sassify" % "1.4.11")
+addSbtPlugin("org.irundaia.sbt" % "sbt-sassify" % "1.5.1")
 
 addSbtPlugin("com.lucidchart" % "sbt-scalafmt" % "1.16")
 
-addSbtPlugin("org.scalastyle" %% "scalastyle-sbt-plugin" % "1.0.0")
+addSbtPlugin("com.beautiful-scala" % "sbt-scalastyle" % "1.5.1")
 
-addSbtPlugin("org.scoverage" % "sbt-scoverage" % "2.0.0")
+addSbtPlugin("org.scoverage" % "sbt-scoverage" % "2.0.7")
 
 addSbtPlugin("com.itv" % "sbt-scalapact" % "3.3.1")
 
