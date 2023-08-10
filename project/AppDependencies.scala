12f686f6049cad986e0ce4e340c795c8c0ccbc8d
garytate123 <garytatelufc@gmail.com>
2023-08-10 15:24:43 +0100

    DDCE-4171 Upgrades deps

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,18 +4,18 @@ import sbt._
 object AppDependencies {
 
   private val pactVersion = "4.4.0"
-  private val bootstrapVersion = "7.15.0"
-  private val akkaVersion = "2.6.20"
+  private val bootstrapVersion = "7.21.0"
+  private val akkaVersion = "2.6.21"
 
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"                 %% "bootstrap-frontend-play-28" % bootstrapVersion,
-    "uk.gov.hmrc"                 %% "auth-client"                % "6.0.0-play-28",
-    "uk.gov.hmrc"                 %% "play-frontend-hmrc"         % "6.2.0-play-28",
+    "uk.gov.hmrc"                 %% "auth-client"                % "6.1.0-play-28",
+    "uk.gov.hmrc"                 %% "play-frontend-hmrc"         % "7.18.0-play-28",
     "uk.gov.hmrc.mongo"           %% "hmrc-mongo-play-28"         % "0.74.0",
-    "com.github.pureconfig"       %% "pureconfig"                 % "0.17.3",
+    "com.github.pureconfig"       %% "pureconfig"                 % "0.17.4",
     "com.beachape"                %% "enumeratum-play"            % "1.7.0",
     "org.webjars.npm"             %  "accessible-autocomplete"    % "2.0.4",
-    "com.softwaremill.quicklens"  %% "quicklens"                  % "1.9.2",
+    "com.softwaremill.quicklens"  %% "quicklens"                  % "1.9.6",
     "org.typelevel"               %% "cats-core"                  % "2.9.0",
     "com.typesafe.akka"           %% "akka-protobuf"              % akkaVersion,
     "com.typesafe.akka"           %% "akka-actor-typed"           % akkaVersion,
@@ -26,11 +26,11 @@ object AppDependencies {
 
   val test: Seq[ModuleID] = (Seq(
     "uk.gov.hmrc"            %% "bootstrap-test-play-28" % bootstrapVersion,
-    "org.scalatest"          %% "scalatest"              % "3.2.15",
+    "org.scalatest"          %% "scalatest"              % "3.2.16",
     "com.typesafe.play"      %% "play-test"              % current,
-    "com.vladsch.flexmark"   %  "flexmark-all"           % "0.64.0",
+    "com.vladsch.flexmark"   %  "flexmark-all"           % "0.64.8",
     "org.scalatestplus.play" %% "scalatestplus-play"     % "5.1.0",
-    "org.scalatestplus"      %% "scalacheck-1-17"        % "3.2.15.0",
+    "org.scalatestplus"      %% "scalacheck-1-17"        % "3.2.16.0",
     "org.scalatestplus"      %% "selenium-4-7"           % "3.2.15.0"
   ) ++ pact ++ mocks).map(_ % Test)
 
