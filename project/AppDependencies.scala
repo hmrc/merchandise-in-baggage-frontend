de29d393dda16180546f7a60cabdfe13a240b0f0
garima-indolia <175022693+garimahmrc@users.noreply.github.com>
2024-08-21 15:20:49 +0100

    DDCE-4771-Toggle-button-Welsh-shift (#537)
    * DDCE-4771-Welsh-toggle-to-follow-design-pattern
    
    * DDCE-4771- Dependency update

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,18 +2,17 @@ import sbt.*
 
 object AppDependencies {
 
-  private val bootstrapVersion = "9.0.0"
+  private val bootstrapVersion = "9.3.0"
 
   private val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"                %% "bootstrap-frontend-play-30" % bootstrapVersion,
-    "uk.gov.hmrc"                %% "play-frontend-hmrc-play-30" % "10.4.0",
-    "uk.gov.hmrc.mongo"          %% "hmrc-mongo-play-30"         % "2.1.0",
-    // need to match playframework version from plugin and bootstrap and this library in this case now it is 3.0.3 everywhere
-    "com.beachape"               %% "enumeratum-play"            % "1.8.0",
+    "uk.gov.hmrc"                %% "play-frontend-hmrc-play-30" % "10.9.0",
+    "uk.gov.hmrc.mongo"          %% "hmrc-mongo-play-30"         % "2.2.0",
+    "com.beachape"               %% "enumeratum-play"            % "1.8.1",
     "org.webjars.npm"             % "accessible-autocomplete"    % "2.0.4",
     "com.softwaremill.quicklens" %% "quicklens"                  % "1.9.7",
     "org.typelevel"              %% "cats-core"                  % "2.12.0",
-    "io.github.samueleresca"     %% "pekko-quartz-scheduler"     % "1.2.1-pekko-1.0.x"
+    "io.github.samueleresca"     %% "pekko-quartz-scheduler"     % "1.2.2-pekko-1.0.x"
   )
 
   private val test: Seq[ModuleID] = Seq(
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -5,9 +5,9 @@ resolvers += Resolver.url("HMRC-open-artefacts-ivy2", url("https://open.artefact
 
 addSbtPlugin("uk.gov.hmrc"        % "sbt-auto-build"           % "3.22.0")
 addSbtPlugin("uk.gov.hmrc"        % "sbt-distributables"       % "2.5.0")
-addSbtPlugin("org.playframework"  % "sbt-plugin"               % "3.0.3")
+addSbtPlugin("org.playframework"  % "sbt-plugin"               % "3.0.5")
 addSbtPlugin("io.github.irundaia" % "sbt-sassify"              % "1.5.2")
 addSbtPlugin("org.scalameta"      % "sbt-scalafmt"             % "2.5.2")
-addSbtPlugin("org.scoverage"      % "sbt-scoverage"            % "2.0.12")
+addSbtPlugin("org.scoverage"      % "sbt-scoverage"            % "2.1.0")
 addSbtPlugin("com.timushev.sbt"   % "sbt-updates"              % "0.6.4")
 addSbtPlugin("uk.gov.hmrc"        % "sbt-accessibility-linter" % "1.0.0")
