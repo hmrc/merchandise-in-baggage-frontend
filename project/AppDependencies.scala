bf3acaf24e811e29b2b8b3a5e59a51097a93fb95
suraj-suseelan <186935751+suraj-suseelan@users.noreply.github.com>
2024-12-17 14:12:24 +0000

    MODs - (Admin Only): Amend Routing for Back Button(dependencies update)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -6,11 +6,11 @@ object AppDependencies {
 
   private val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"                %% "bootstrap-frontend-play-30" % bootstrapVersion,
-    "uk.gov.hmrc"                %% "play-frontend-hmrc-play-30" % "11.5.0",
+    "uk.gov.hmrc"                %% "play-frontend-hmrc-play-30" % "11.7.0",
     "uk.gov.hmrc.mongo"          %% "hmrc-mongo-play-30"         % "2.3.0",
     "com.beachape"               %% "enumeratum-play"            % "1.8.2",
     "org.webjars.npm"             % "accessible-autocomplete"    % "3.0.0",
-    "com.softwaremill.quicklens" %% "quicklens"                  % "1.9.9",
+    "com.softwaremill.quicklens" %% "quicklens"                  % "1.9.10",
     "org.typelevel"              %% "cats-core"                  % "2.12.0",
     "io.github.samueleresca"     %% "pekko-quartz-scheduler"     % "1.2.2-pekko-1.0.x"
   )
