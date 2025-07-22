3174f2f8926ffb9a6b505c753b045c366e978a5b
Suraj Suseelan <186935751+suraj-suseelan@users.noreply.github.com>
2025-07-22 09:28:16 +0100

    DDCE-6493: MODS - merchandise-in-baggage-frontend: Transform <select> Elements into Accessible Autocomplete (#552)
    * DDCE-6493: MODS - merchandise-in-baggage-frontend: Transform <select> Elements into Accessible Autocomplete
    
    * DDCE-6493: MODS - merchandise-in-baggage-frontend: Transform <select> Elements into Accessible Autocomplete
    
    * DDCE-6493: MODS - merchandise-in-baggage-frontend: Transform <select> Elements into Accessible Autocomplete
    
    * DDCE-6493: MODS - merchandise-in-baggage-frontend: Transform <select> Elements into Accessible Autocomplete
    
    * DDCE-6493: MODS - merchandise-in-baggage-frontend: Transform <select> Elements into Accessible Autocomplete

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,13 +2,13 @@ import sbt.*
 
 object AppDependencies {
 
-  private val bootstrapVersion = "9.12.0"
+  private val bootstrapVersion = "9.16.0"
 
   private val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"                %% "bootstrap-frontend-play-30" % bootstrapVersion,
-    "uk.gov.hmrc"                %% "play-frontend-hmrc-play-30" % "12.1.0",
-    "uk.gov.hmrc.mongo"          %% "hmrc-mongo-play-30"         % "2.6.0",
-    "com.beachape"               %% "enumeratum-play"            % "1.8.2",
+    "uk.gov.hmrc"                %% "play-frontend-hmrc-play-30" % "12.7.0",
+    "uk.gov.hmrc.mongo"          %% "hmrc-mongo-play-30"         % "2.7.0",
+    "com.beachape"               %% "enumeratum-play"            % "1.9.0",
     "org.webjars.npm"             % "accessible-autocomplete"    % "3.0.1",
     "com.softwaremill.quicklens" %% "quicklens"                  % "1.9.12",
     "org.typelevel"              %% "cats-core"                  % "2.13.0",
