308058eb46509f60008fac46e4f54ad49b95bad3
Gaston Racui <gastonracui@gmail.com>
2026-06-05 13:17:17 +0100

    DDCE-8556 Migration changes
    DDCE-8556 Migration changes

--- app/uk/gov/hmrc/merchandiseinbaggage/config/AppConfig.scala
+++ app/uk/gov/hmrc/merchandiseinbaggage/config/AppConfig.scala
@@ -63,9 +63,6 @@ class AppConfig @Inject() (
   lazy val merchandiseInBaggageUrl: String  = servicesConfig.baseUrl("merchandise-in-baggage")
   lazy val addressLookupFrontendUrl: String = servicesConfig.baseUrl("address-lookup-frontend")
 
-  lazy val forceNavigation: Boolean =
-    config.getOptional[Boolean]("play-frontend-hmrc.forceServiceNavigation").getOrElse(false)
-
   def addressLookupCallbackUrl(isAssistedDigital: Boolean): String =
     if (isAssistedDigital) {
       config.get[String]("microservice.services.address-lookup-frontend.adminCallback")
